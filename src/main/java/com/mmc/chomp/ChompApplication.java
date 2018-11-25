7329c054082fa892afe1ca8b17be747e5bb57114
Mateusz Cieplak <cieplakm@gmail.com>
2018-11-25 17:07:38 +0100

    init

---
+++ src/main/java/com/mmc/chomp/ChompApplication.java
@@ -0,0 +1,12 @@
+package com.mmc.chomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChompApplication.class, args);
+	}
+}
